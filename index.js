7b86cd642a6f75cae3725035263c79a70e1e4073
samjeffcoat <samjeffcoat@gmail.com>
2019-03-04 15:26:48 -0600

    Moves migrations folder because it wasnt working, data migrated to sqlite3, able to register a user in postman

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const helmet= require('helmet');
 
  const db= require('./database/dbConfig.js');
 
+ const Users = require('./users/users-module');
  const server= express();
 
  server.use(helmet());
@@ -15,6 +16,19 @@ server.get('/', (req, res) => {
     res.send("Its working!!!!!!");
 })
 
+server.post('/api/register', (req, res) => {
+    let user=req.body;
+
+    const hash= bcrypt.hashSync(user.password, 12)
+
+    Users.add(user)
+    .then(saved => {
+        res.status(201).json(saved);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+})
 
 
 
