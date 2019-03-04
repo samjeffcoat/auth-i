a09320a14a2c305da64eedc63367a3cb792b38e3
samjeffcoat <samjeffcoat@gmail.com>
2019-03-04 14:49:39 -0600

    Adds server and it is working

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const helmet= require('helmet');
  const cors = require('cors');
  const bcrypt = require('bcryptjs');
 
- const db= require('./database/dbUsers.js');
+ const db= require('./database/dbConfig.js');
+
+ const server= express();
+
+ server.use(helmet());
+ server.use(express.json());
+ server.use(cors());
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
