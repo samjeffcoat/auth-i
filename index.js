cc6911ac431b056e73945492ac67346a2209b821
samjeffcoat <samjeffcoat@gmail.com>
2019-03-05 14:50:49 -0600

    Sets up new restricted function

--- index.js
+++ index.js
@@ -2,17 +2,29 @@ const express = require('express');
 const helmet= require('helmet');
  const cors = require('cors');
  const bcrypt = require('bcryptjs');
-
+// Step 1 
  const session = require('express-session');
 
  const db= require('./database/dbConfig.js');
 
  const Users = require('./users/users-module');
  const server= express();
+ const sessionConfig = {
+     name: 'Baby Bear',
+     secret: 'Keep it secret, keep it safe!',
+     cookie: {
+         maxAge:1000 *60*15*24, //// This is set to 24 hours 
+         secure:false,
+     },
+     httpOnly:true,
+     resave:false,
+     saveUninitialized: false, 
+ }
 
  server.use(helmet());
  server.use(express.json());
  server.use(cors());
+ server.use(session(sessionConfig))// Step 2
 
 server.get('/', (req, res) => {
     res.send("Its working!!!!!!");
@@ -49,9 +61,9 @@ server.post('/api/login', (req, res) => {
         res.status(500).json(error);
     })
 })
-
+/*
 function restricted(req, res, next) {
-    const {username, password} = req.headers
+   const {username, password} = req.headers
 
     if(username && password){
         Users.findBy({username})
@@ -71,6 +83,17 @@ function restricted(req, res, next) {
         res.status(400).json({message: "No creds provided"})
     }
 }
+*/
+//4 new simplier restricted function
+
+function restricted(req, res, next){
+    if(req.session && req.session.username){
+        next()
+    } else{
+        res.status(401).json({message: 'You shall not pass!'})
+    }
+}
+
 
 server.post('/api/users', restricted, (req, res) => {
     Users.find()
